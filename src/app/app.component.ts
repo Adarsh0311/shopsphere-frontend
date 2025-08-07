a212afe3fc896fcc52e21daffb7724e70979ce04
adarsh0311 <adarshghai6@gmail.com>
2025-08-07 02:51:53 -0700

    setup environment files for the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'shopsphere-frontend';
+  title = environment.appName;
+
+    constructor() {
+    if (environment.enableLogging) {
+      console.log('App started in development mode');
+      console.log('API URL:', environment.apiUrl);
+      console.log('Version:', environment.version);
+    }
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    appName: 'ShopSphere',
+    apiUrl: 'http://localhost:8080/api',
+    version: '1.0.0',
+    enableLogging: true
+}
